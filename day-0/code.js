667ce9f9e59fc4dc0c130866008cb07be9284e96
Matt Newman <git@mattnewman.me>
2020-12-15 20:42:10 +0000

    Day 5, part 1: rows seem to be working

--- day-0/code.js
+++ day-0/code.js
@@ -1,13 +1,37 @@
-const getSeat = ({
+const getAllSeats = ({
     input = []
+}) => {
+    // for dealing with the full array
+}
+
+const getSeat = ({
+    input = ''
 }) => {
     let row = 0;
     let column = 0;
-    const byteMap = [64, 32, 16, 8, 4, 2, 1];
     const regex = /^([FB]{7})([LR]{3})$/i;
+
+    const data = input.match(regex);
+    const rowData = data[1].split('');
+    const columnData = data[2].split('');
+
+    row = getRow(rowData);
+    column = getColumn(columnData);
 
     return { row, column };
 };
+
+const getRow = ( rowData=[] ) => {
+    const byteMap = [64, 32, 16, 8, 4, 2, 1];
+    const rows = 127; // 128 but 0 indexed
+    return rowData.reduce( (acc, current, index) => {
+        return current === 'F' ? acc - byteMap[index] : acc;
+    }, 127);
+}
+
+const getColumn = ( columnData=[] ) => {
+    return 0;
+}
 
 const getSeatId = ({
     seat
@@ -17,5 +41,6 @@ const getSeatId = ({
 
 export {
     getSeat,
-    getSeatId
+    getSeatId,
+    getAllSeats,
 };
--- day-05/day.spec.js
+++ day-05/day.spec.js
@@ -1,16 +1,13 @@
 import should from 'should';
-import {
-    getSeat,
-    getSeatId
-} from '../day-0/code.js';
 
 import {
     getContents
 } from '../utils/readFile.js';
 
 import {
-    a
-} from './code.js';
+    getSeat,
+    getSeatId
+} from '../day-0/code.js';
 
 let input = getContents('./day-05/example.txt').split('\n');
 
@@ -19,7 +16,7 @@ describe("day 05", () => {
     describe("part 1", () => {
         it("should return the correct answer matching the example", () => {
             const result = getSeat({
-                input
+                input: input[0]
             });
 
             result.should.deepEqual({
